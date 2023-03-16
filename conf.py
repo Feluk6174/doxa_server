694bc3589d1a3ae2d4c6116cf4c63ad2ebe1cd8e
Guillem Saiz Pascual <25729908+gsaiz@users.noreply.github.com>
2023-03-16 19:41:17 +0100

    feat: initial dockerisation

--- conf.py
+++ conf.py
@@ -1,12 +1,31 @@
 import json
+import os
 
-def init():
-    global conf
+def read_config_from_file():
     with open("conf.json", "r") as f:
         str_conf = f.read()
         if str_conf == "" or str_conf == "{}":
             raise EmptyConf()
         conf = json.loads(str_conf)
+    return conf
+
+def read_config_from_env():
+    try:
+        conf = {
+            "database": {
+                "host": os.environ["MYSQL_HOST"],
+                "user": os.environ["MYSQL_USER"],
+                "password": os.environ["MYSQL_PASSWORD"],
+                "database": os.environ["MYSQL_DATABASE"],
+            }
+        }
+    except KeyError:
+        raise EmptyConf
+    return conf
+
+def init():
+    global conf
+    conf = read_config_from_env()
 
 def api(parameter):
     global conf
